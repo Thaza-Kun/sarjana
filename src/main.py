380ba1e19fe1b65e10a6f4848f9d7f73f60b5391
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-09-23 12:15:09 +0800

    refactor: add FRBPopulation class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import asyncio
-import fetch
+from source.frbstats import FRBPopulation
 
 if __name__ == "__main__":
-    data = asyncio.run(fetch.frbstats.get_frb_population_data_as_csv())
-    print(data)
+    data = FRBPopulation()
+    print(data.dataframe)
---
+++ src/source/__init__.py
@@ -0,0 +1,2 @@
+from . import frbstats
+from . import external
