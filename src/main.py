44225adaf26d8bfc3e8303ce4207446faaa19a9a
Thaza-Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-11-17 16:08:17 +0800

    ♻ REFACTOR: load data is in sources

--- src/main.py
+++ src/main.py
@@ -2,10 +2,7 @@ import workflows
 
 if __name__ == "__main__":
     seed = 42
-    for cluster_size in range(2, 50):
-        workflows.replicate_chen2021_UMAP_HDBSCAN(
-            min_cluster_size=cluster_size, seed=seed
-        )
-        workflows.UMAP_HDBSCAN_no_model_dependent_params(
-            min_cluster_size=cluster_size, seed=seed
-        )
+    min_cluster_size = 19
+    workflows.replicate_chen2021_UMAP_HDBSCAN(
+        min_cluster_size=min_cluster_size, seed=seed
+    )
