0d97041ee2c2fa0bfac77396d156f0306a5dca4a
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-05-30 16:31:25 +0800

    ⚙ FIX: Remove RFI as default

--- sarjana/handlers.py
+++ sarjana/handlers.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 from copy import deepcopy
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 import h5py
 import pandas as pd
@@ -32,12 +34,109 @@ class ParquetWaterfall:
         columns: Optional[List[str]] = None,
     ) -> None:
         """
-        Read Waterfall data from parquet.
+        Read Waterfall data from parquet and remove RFI.
         """
         self.filename = filename
         self.dataframe = pd.read_parquet(filename, columns=columns, engine="pyarrow")
         self._unpack()
+        self.remove_rfi()
         self.wfall_dimension = "freq", "time"
+
+    def plot_wfall(self, scatterfit: bool = False) -> "ParquetWaterfall":
+        self.remove_rfi()
+        extent = [*self.extent]
+        fig = plt.figure(figsize=(6,6))
+        ## Set up the image grid
+        gs = gridspec.GridSpec(ncols=2, nrows=2, figure=fig, width_ratios=[3, 1],
+                                height_ratios=[1, 3], hspace=0.0, wspace=0.0)
+        data_im = plt.subplot(gs[2])
+        data_ts = plt.subplot(gs[0], sharex=data_im)
+        data_spec = plt.subplot(gs[3], sharey=data_im)
+
+
+        ### time stamps relative to the peak
+        peak_idx = np.argmax(self.ts)
+        extent[0] = self.extent[0] - self.plot_time[peak_idx]
+        extent[1] = self.extent[1] - self.plot_time[peak_idx]
+        plot_time = self.plot_time.copy() - self.plot_time[peak_idx]
+
+        # prepare time-series for histogramming
+        plot_time -= self.dt / 2.
+        plot_time = np.append(plot_time, plot_time[-1] + self.dt)
+
+        cmap = plt.cm.viridis
+
+        ### plot dynamic spectrum
+        self.wfall[np.isnan(self.wfall)] = np.nanmedian(self.wfall)   # replace nans in the data with the data median
+        # use standard deviation of residuals to set color scale
+        vmin = np.nanpercentile(self.wfall, 1)
+        vmax = np.nanpercentile(self.wfall, 99)
+
+        data_im.imshow(self.wfall, aspect="auto", interpolation="none",
+                        extent=extent, vmin=vmin, vmax=vmax, cmap=cmap)
+
+        ### plot time-series
+        data_ts.plot(plot_time, np.append(self.ts, self.ts[-1]), color="tab:gray",
+                        drawstyle="steps-post")
+
+        ### plot spectrum
+        data_spec.plot(self.spec, self.plot_freq, color="tab:gray")
+
+        ### plot model time-series and spectrum
+        if scatterfit:
+            data_spec.plot(self.model_spec, self.plot_freq, color=cmap(0.25))
+            data_ts.plot(plot_time, np.append(self.model_ts, self.model_ts[-1]),
+                            color=cmap(0.25), drawstyle="steps-post", lw=2)
+        else:
+            data_spec.plot(self.model_spec, self.plot_freq, color=cmap(0.5))
+            data_ts.plot(plot_time, np.append(self.model_ts, self.model_ts[-1]),
+                            color=cmap(0.5), drawstyle="steps-post", lw=1)
+
+
+        ## BEautify plot
+        # remove some labels and ticks for neatness
+        plt.setp(data_ts.get_xticklabels(), visible=False)
+        data_ts.set_yticklabels([], visible=True)
+        data_ts.set_yticks([])
+        data_ts.set_xlim(extent[0], extent[1])
+        plt.setp(data_spec.get_yticklabels(), visible=False)
+        data_spec.set_xticklabels([], visible=True)
+        data_spec.set_xticks([])
+        data_spec.set_ylim(extent[2], extent[3])
+        plt.setp(data_im.get_xticklabels(), fontsize=9)
+        plt.setp(data_im.get_yticklabels(), fontsize=9)
+
+
+        # #### highlighting the width of the pulse
+        # data_ts.axvspan(max(plot_time.min(),
+        #                         plot_time[peak] + 0.5 * self.dt \
+        #                         - (0.5 * width) * self.dt),
+        #                     min(plot_time.max(),
+        #                         plot_time[peak] + 0.5 * self.dt \
+        #                         + (0.5 * width) * self.dt),
+        #                     facecolor="tab:blue", edgecolor=None, alpha=0.1)
+
+
+        ##### add event ID and DM labels
+        xlim = data_ts.get_xlim()
+        ylim = data_ts.get_ylim()
+
+        # add 20% extra white space at the top
+        span = np.abs(ylim[1]) + np.abs(ylim[0])
+        data_ts.set_ylim(ylim[0], ylim[1] + 0.2 * span)
+        ylim = data_ts.get_ylim()
+
+        ypos = (ylim[1] - ylim[0]) * 0.9 + ylim[0]
+        xpos = (xlim[1] - xlim[0]) * 0.98 + self.extent[0]
+        # data_ts.text(xpos, ypos, "{}\nDM: {:.1f} pc/cc\nSNR: {:.2f}".format(self.eventname, self.dm,self.snr), ha="right",
+        data_ts.text(xpos, ypos, "{}\nDM: {:.1f} pc/cc".format(self.eventname, self.dm), ha="right",
+                        va="top", fontsize=9)
+
+        data_im.locator_params(axis="x", min_n_ticks=3)
+        data_im.set_yticks([400, 500, 600, 700, 800])
+        data_im.set_ylabel("Frequency [MHz]", fontsize=9)
+        data_im.set_xlabel("Time [ms]", fontsize=9)
+
 
     def transpose(self) -> "ParquetWaterfall":
         self.wfall = np.transpose(self.wfall)
