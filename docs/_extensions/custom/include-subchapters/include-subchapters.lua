8f42aa2e9c3771ad588a74478f40f94b38c1a6e3
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-11-01 20:30:37 +0800

    :book: DOCS: Conclusion

--- docs/_extensions/custom/include-subchapters/include-subchapters.lua
+++
@@ -1,8 +0,0 @@
-function Div(el)
-    if el.classes:includes('as-subchapter') then
-        return el.content:walk{
-                Header = function(el) el.level = el.level + 1 return el end
-            }
-    end
-    return el
-end
